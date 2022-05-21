c1fce80b2fd8beeb8ce5a682a512081b022d73d2
OSTO92 <99026122+OSTO92@users.noreply.github.com>
2022-05-21 13:51:42 +0700

    Update counting_for.hpp

--- src/counting_for.hpp
+++ src/counting_for.hpp
@@ -1,4 +1,39 @@
 #ifndef COMPARISON_H
 #define COMPARISON_H
+int counting_for_tele2(int* imax_fortele2, int* max_fortele2, int* tele2, int* mts, int* megafon, int* beeline, int* yota);
+int counting_for_mts(
+        int* imax_mts,
+        int* max_mts,
+        int* tele2,
+        int* mts,
+        int* megafon,
+        int* beeline,
+        int* yota);
+int counting_for_megafon(
+        int* imax_megafon,
+        int* max_megafon,
+        int* tele2,
+        int* mts,
+        int* megafon,
+        int* beeline,
+        int* yota);
+int counting_for_beeline(
+        int* imax_beeline,
+        int* max_beeline,
+        int* tele2,
+        int* mts,
+        int* megafon,
+        int* beeline,
+        int* yota);
+int counting_for_yota(
+        int* imax_yota,
+        int* max_yota,
+        int* tele2,
+        int* mts,
+        int* megafon,
+        int* beeline,
+        int* yota);
+
+
 int comparison(int* tele2, int* mts, int* megafon, int* beeline, int* yota);
 #endif

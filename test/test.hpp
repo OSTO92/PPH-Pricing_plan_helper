e3cca607b61bb6a10f709c4991e25466290e50cf
OSTO92 <99026122+OSTO92@users.noreply.github.com>
2022-05-21 14:04:02 +0700

    Create test.hpp

---
+++ test/test.hpp
@@ -0,0 +1,8 @@
+#ifndef TEST_H
+#define TEST_H
+int counting_for_tele2(&imax, &max, tele2, m, b, c, e);
+int counting_for_mts(&imax, &max, tele2, m, b, c, e);
+int counting_for_megafon(&imax, &max, tele2, m, b, c, e);
+int counting_for_beeline(&imax, &max, tele2, m, b, c, e);
+int counting_for_yota(&imax, &max, tele2, m, b, c, e);
+#endif

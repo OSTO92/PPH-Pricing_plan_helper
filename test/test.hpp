132eb2199cf498ca9b5ad0815c541829486d479f
OSTO92 <99026122+OSTO92@users.noreply.github.com>
2022-05-21 14:09:49 +0700

    Update test.hpp

--- test/test.hpp
+++ test/test.hpp
@@ -1,8 +1,8 @@
 #ifndef TEST_H
 #define TEST_H
-int counting_for_tele2(&imax, &max, tele2, m, b, c, e);
-int counting_for_mts(&imax, &max, tele2, m, b, c, e);
-int counting_for_megafon(&imax, &max, tele2, m, b, c, e);
-int counting_for_beeline(&imax, &max, tele2, m, b, c, e);
-int counting_for_yota(&imax, &max, tele2, m, b, c, e);
+int counting_for_tele2(int* imax, int* max, int tele2, int m, int b, int c, int e);
+int counting_for_mts(int* imax, int* max, int tele2, int m, int b, int c, int e);
+int counting_for_megafon(int* imax, int* max, int tele2, int m, int b, int c, int e);
+int counting_for_beeline(int* imax, int* max, int tele2, int m, int b, int c, int e);
+int counting_for_yota(int* imax, int* max, int tele2, int m, int b, int c, int e);
 #endif
